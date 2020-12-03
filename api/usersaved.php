9c8e82b7f21cd6c45be27d9e21f4894754fccb19
ZaneGarrison <zane.garrison@gmail.com>
2020-12-02 19:18:17 -0500

    Created User Saved

---
+++ api/usersaved.php
@@ -0,0 +1,41 @@
+<?php
+header("Content-Type:application/json");
+$host = "database-1.c7mdfiikfgpx.us-east-1.rds.amazonaws.com";
+$username ="admin";
+$password ="myawsdatabase20!";
+$dbname = "studentdb";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (mysqli_connect_error()) {
+    die('Failed');
+}
+
+$currentUsername = $_COOKIE["user"];
+
+$userSaved = "SELECT * FROM savedBook WHERE 
+ 	pages = (SELECT username($currentUsername) FROM savedBook)";
+$result = mysqli_query($conn, $userSaved);
+if($row = mysqli_fetch_assoc($result)){
+    $title = $row['title'];
+    $authors = $row['authors'];
+    $publisher = $row['publisher'];
+    $pages = $row['pages'];
+    $date = $row['date'];
+    $cover = $row['cover'];
+    $username = $row['username'];
+    response($title, $authors, $publisher, $pages, $date, $cover,$username);
+}
+
+
+function response($title, $authors, $publisher, $pages, $date, $cover,$username) {
+    $response['title'] = $title;
+    $response['authors'] = $authors;
+    $response['publisher'] = $publisher;
+    $response['pages'] = $pages;
+    $response['date'] = $date;
+    $response['cover'] = $cover;
+    $username['username'] = $username;
+    $json = json_encode($response);
+    echo $json;
+}
+?>
--- create.php
+++ create.php
@@ -1,28 +1,27 @@
-
 <?php
-	$user = $_POST["user"];
-	$pass = $_POST["pass"];
-	$host = "database-1.c7mdfiikfgpx.us-east-1.rds.amazonaws.com";
-	$username ="admin";
-	$password ="myawsdatabase20!";
-	$dbname = "studentdb";
+$user = $_POST["user"];
+$pass = $_POST["pass"];
+$host = "database-1.c7mdfiikfgpx.us-east-1.rds.amazonaws.com";
+$username ="admin";
+$password ="myawsdatabase20!";
+$dbname = "studentdb";
 
-	$conn = mysqli_connect($host, $username, $password, $dbname);
-	if (mysqli_connect_error()) {
-		die('Failed');
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (mysqli_connect_error()) {
+	die('Failed');
+}
+$select = "SELECT * FROM bookAccount WHERE username = '$user'";
+$results = mysqli_query($conn, $select);
+if (!mysqli_num_rows($results) > 0) {
+	$insert = "INSERT INTO bookAccount (username, password) VALUES ('$user', '$pass')";
+	if (mysqli_query($conn, $insert)) {
+		header("Location: login_page.html");
 	}
-	$select = "SELECT * FROM bookAccount WHERE username = '$user'";
-	$results = mysqli_query($conn, $select);
-  	if (!mysqli_num_rows($results) > 0) {
-  		$insert = "INSERT INTO bookAccount (username, password) VALUES ('$user', '$pass')";
-		if (mysqli_query($conn, $insert)) {
-			header("Location: login_page.html");
-		}
-	}
-	else {
-  		header("Location: create_page.html");
-  	}
-	
+}
+else {
+	header("Location: create_page.html");
+}
 
-	mysqli_close($conn);
+
+mysqli_close($conn);
 ?>
